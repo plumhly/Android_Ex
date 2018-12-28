6bfd5b79a357f652470a27eddb00a21cd53bfdaf
plum <>
2018-12-28 16:34:12 +0800

    Android练习

---
+++ Quiz/app/src/main/java/com/example/plum/quiz/QuizActivity.java
@@ -0,0 +1,15 @@
+package com.example.plum.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static com.example.plum.quiz.R.layout.activity_quiz;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_quiz);
+    }
+}
