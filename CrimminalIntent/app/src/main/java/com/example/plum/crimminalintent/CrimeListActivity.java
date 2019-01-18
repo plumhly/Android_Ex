3330e323b3cf9bbe0786fcf75d752c3684854b6c
plum <>
2019-01-18 16:05:48 +0800

    学习完第8章

---
+++ CrimminalIntent/app/src/main/java/com/example/plum/crimminalintent/CrimeListActivity.java
@@ -0,0 +1,10 @@
+package com.example.plum.crimminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
