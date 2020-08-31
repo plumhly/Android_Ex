8aeaf737e79c256cc86a81fc779683cd17046a3f
libo <bo.li@heywoof.com>
2020-08-31 18:40:04 +0800

    feat: 替换image button

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +15,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
-    private Button mNextButton;
-    private Button mPreButton;
+    private ImageButton mNextButton;
+    private ImageButton mPreButton;
 
     private Question[] mQuestionBank = {
             new Question(R.string.question_australia, true),
