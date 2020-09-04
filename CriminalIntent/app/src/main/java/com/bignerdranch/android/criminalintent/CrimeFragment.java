d9b9e227643991aa8e8f94ab083001c30db0b245
libo <bo.li@heywoof.com>
2020-09-04 18:52:43 +0800

    feat: 完成练习

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -21,6 +21,7 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ShareCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -29,6 +30,7 @@ import java.net.URI;
 import java.util.Date;
 import java.util.UUID;
 
+import static androidx.core.app.ShareCompat.*;
 import static java.text.DateFormat.*;
 
 public class CrimeFragment extends Fragment {
@@ -112,11 +114,12 @@ public class CrimeFragment extends Fragment {
         mSendMsgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_suspect));
-                i = Intent.createChooser(i, getString(R.string.send_report));
+                Intent i = IntentBuilder.from(getActivity())
+                        .setType("text/plain")
+                        .setText(getCrimeReport())
+                        .setSubject(getString(R.string.crime_report_subject))
+                        .setChooserTitle(R.string.send_report)
+                        .getIntent();
                 startActivity(i);
             }
         });
