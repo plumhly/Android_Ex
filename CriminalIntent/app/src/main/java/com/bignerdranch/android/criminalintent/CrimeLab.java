db14bea6420010941539720680be0fed454a9912
libo <bo.li@heywoof.com>
2020-09-07 11:37:31 +0800

    feat: 学习完16章

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -107,6 +108,11 @@ public class CrimeLab {
         } finally {
             cursorWrapper.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 }
 
