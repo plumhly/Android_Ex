8ed09b35b387aa2f8841139e56671cc9e5028e14
libo <bo.li@heywoof.com>
2020-09-09 17:17:35 +0800

    feat: 学习31章

---
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.draganddraw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+}
