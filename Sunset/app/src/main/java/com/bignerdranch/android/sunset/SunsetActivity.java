76e9cc23d67b6a755852d783619b3b76d01a1c5e
libo <bo.li@heywoof.com>
2020-09-09 17:48:14 +0800

    feat: 学习完32章

---
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/SunsetActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.sunset;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class SunsetActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
